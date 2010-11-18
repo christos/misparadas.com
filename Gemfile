048209d5b16cc39005eae652c770b5c5c76e4f6b
Sam Lown <me@samlown.com>
2010-11-18 11:31:53 +0100

    Upgrading and preparing for bundler

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source :gemcutter
+
+gem "rails", "~> 2.3.5"
+
+gem "hpricot"
+gem "geokit"
+gem "haml"
+
+group :development do
+  gem "sqlite3-ruby", :require => "sqlite3"
+  gem "ruby-debug"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -23,10 +23,10 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "hpricot"
-  config.gem "geokit"
-  config.gem "haml"
-  config.gem "rack", "~>1.0.1"
+  #config.gem "hpricot"
+  #config.gem "geokit"
+  #config.gem "haml"
+  #config.gem "rack", "~>1.0.1"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -51,5 +51,9 @@ end
 Haml::Template.options[:format] = :html5
 
 ActiveRecord::Base.include_root_in_json = false
+
+if Rails.env == 'production'
+  Sass::Plugin.options[:never_update] = true
+end
 
 require 'core'
